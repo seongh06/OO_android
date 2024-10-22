97d287123ea75b6fbe068fdb577d85ca5ba9f712
EthanolSwab <y00000nje0ng@gmail.com>
2024-10-22 21:59:53 +0900

    [UI/#7] 홈제작중

--- app/src/main/java/ggum/oo/presentation/home/HomeFragment.kt
+++ app/src/main/java/ggum/oo/presentation/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package ggum.oo.presentation.home
 
 import androidx.navigation.NavController
+import androidx.viewpager2.widget.ViewPager2
 import dagger.hilt.android.AndroidEntryPoint
 import ggum.oo.R
 import ggum.oo.databinding.FragmentHomeBinding
@@ -9,11 +10,21 @@ import ggum.oo.presentation.base.BaseFragment
 @AndroidEntryPoint
 class HomeFragment: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private lateinit var navController: NavController
-    override fun initView() {
+    override fun initView(
 
+
+    ) : View {
+
+        val bannerAdapter = HomeBannerVPA(this)
+        bannerAdapter.addFragment(HomeBannerFragment())
+        binding.HomeBannerVPA.adapter = bannerAdapter
+        binding.HomeBannerVPA.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        return binding.root
     }
 
     override fun initObserver() {
 
     }
+
 }
